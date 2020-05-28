ee0248eedc2c88f988ff83e223641b5de66eea3d
Tim Hockin <thockin@google.com>
2020-05-27 22:06:16 -0700

    Get rid of InfoLogger, implement V() as additive

--- example/main.go
+++ example/main.go
@@ -37,7 +37,7 @@ func main() {
 	log = log.WithName("MyName").WithValues("user", "you")
 	log.Info("hello", "val1", 1, "val2", map[string]int{"k": 1})
 	log.V(1).Info("you should see this")
-	log.V(3).Info("you should NOT see this")
+	log.V(1).V(1).Info("you should NOT see this")
 	log.Error(nil, "uh oh", "trouble", true, "reasons", []float64{0.1, 0.11, 3.14})
 	log.Error(E{"an error occurred"}, "goodbye", "code", -1)
 }
