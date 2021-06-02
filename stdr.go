cabd927d891e0a61fa469e327da32646891be47b
Tim Hockin <thockin@google.com>
2021-06-02 14:46:32 -0700

    Simplify With* receivers

--- stdr.go
+++ stdr.go
@@ -370,34 +370,28 @@ func (l logger) output(calldepth int, s string) {
 // WithName returns a new logr.Logger with the specified name appended.  stdr
 // uses '/' characters to separate name elements.  Callers should not pass '/'
 // in the provided name string, but this library does not actually enforce that.
-func (l *logger) WithName(name string) logr.LogSink {
-	l2 := &logger{}
-	*l2 = *l
-	if len(l2.prefix) > 0 {
-		l.prefix = l2.prefix + "/"
+func (l logger) WithName(name string) logr.LogSink {
+	if len(l.prefix) > 0 {
+		l.prefix = l.prefix + "/"
 	}
-	l2.prefix += name
-	return l2
+	l.prefix += name
+	return &l
 
 }
 
 // WithValues returns a new logr.Logger with the specified key-and-values
 // saved.
-func (l *logger) WithValues(kvList ...interface{}) logr.LogSink {
-	l2 := &logger{}
-	*l2 = *l
+func (l logger) WithValues(kvList ...interface{}) logr.LogSink {
 	// Three slice args forces a copy.
 	n := len(l.values)
-	l2.values = append(l2.values[:n:n], kvList...)
-	return l2
+	l.values = append(l.values[:n:n], kvList...)
+	return &l
 
 }
 
-func (l *logger) WithCallDepth(depth int) logr.LogSink {
-	l2 := &logger{}
-	*l2 = *l
-	l2.depth += depth
-	return l2
+func (l logger) WithCallDepth(depth int) logr.LogSink {
+	l.depth += depth
+	return &l
 }
 
 // Underlier exposes access to the underlying logging implementation.  Since
